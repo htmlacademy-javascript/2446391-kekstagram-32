4e449cd5baf8203b0fae46e5ac40ffb60dfa87cb
Keks <keks@htmlacademy.ru>
2024-07-25 15:16:56 +0300

    :heavy_check_mark: Сборка #20

---
+++ 20/js/main.js
@@ -0,0 +1,6 @@
+import { arrayPictures } from './data-generate.js';
+import {similarPictures} from './render-thumbnail.js';
+import { renderBigPicture } from './big-picture.js';
+
+similarPictures(arrayPictures);
+renderBigPicture(arrayPictures);
---
+++ 20/js/util.js
@@ -0,0 +1,13 @@
+const getRandom = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+
+export { getRandom, isEnterKey, isEscapeKey };
